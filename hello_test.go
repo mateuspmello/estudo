1ab681638424d4bbb40e0286d9d7c8c710f8c6ef
mateuspmello <mateuspmello@gmail.com>
2020-06-18 23:28:22 -0300

    refatorando o codigo 2

--- hello_test.go
+++ hello_test.go
@@ -14,15 +14,29 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Testando saida do hello", func(t *testing.T) {
-		got := Hello("Mateus")
+		got := Hello("Mateus", "")
 		want := "Hello, Mateus"
 
 		mensagemDeErro(t, got, want)
 	})
 
 	t.Run("Testando com nome vazio", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		mensagemDeErro(t, got, want)
+	})
+
+	t.Run("Testando com lingua portuguesa", func(t *testing.T) {
+		got := Hello("Mateus", "Portugues")
+		want := "Olá, Mateus"
+
+		mensagemDeErro(t, got, want)
+	})
+
+	t.Run("Testando com lingua italiana", func(t *testing.T) {
+		got := Hello("Mateus", "Italiano")
+		want := "Ciao, Mateus"
 
 		mensagemDeErro(t, got, want)
 	})
--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import "fmt"
 
+const portugues = "Portugues"
+const italiano = "Italiano"
+
 const helloEnglish = "Hello, "
+const olaPortugues = "Olá, "
+const ciaoItaliano = "Ciao, "
 const helloWorld = "World"
 
 //Hello dá as boa vindas
-func Hello(nome string) string {
+func Hello(nome string, lingua string) string {
 	if nome == "" {
 		nome = helloWorld
 	}
-	return helloEnglish + nome
+	return prefixo(lingua) + nome
+}
 
+func prefixo(lingua string) (prefixo string) {
+	switch lingua {
+	case portugues:
+		prefixo = olaPortugues
+	case italiano:
+		prefixo = ciaoItaliano
+	default:
+		prefixo = helloEnglish
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Mateus"))
+	fmt.Println(Hello("Mateus", ""))
 }
