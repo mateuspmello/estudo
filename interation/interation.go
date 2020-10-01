ed377390ca8721d3e53726678a5c64f2834a69da
mateuspmello <mateuspmello@gmail.com>
2020-09-30 23:50:55 -0300

    finalizando testes

--- interation/interation.go
+++ interation/interation.go
@@ -1,9 +1,10 @@
 package interation
 
 //Imprime imprime as 5 primeiras letras
-func Imprime(a string) string {
-	for i := 0; i < 4; i++ {
-		a += "a"
+func Imprime(a string, b int) string {
+	var texto string
+	for i := 0; i < b; i++ {
+		texto += a
 	}
-	return a
+	return texto
 }
--- interation/interation_test.go
+++ interation/interation_test.go
@@ -4,11 +4,17 @@ import "testing"
 
 func TestImprimirCincoLetras(t *testing.T) {
 	t.Run("Imprimir Cinco Letras", func(t *testing.T) {
-		want := "aaaaa"
-		got := Imprime("a")
+		got := Imprime("a", 3)
+		want := "aaa"
 
 		if want != got {
 			t.Errorf("Esperava %s mas recebeu %s", want, got)
 		}
 	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Imprime("a", 6)
+	}
 }
